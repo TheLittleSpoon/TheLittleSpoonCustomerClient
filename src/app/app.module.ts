bb07a04508093c0838aafc594cc07dffcf55be83
Addie Cohen <dushkit100@gmail.com>
2021-02-16 23:11:36 +0200

    homepage almost done, need to add sidebar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import {CommonModule} from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {HttpClientModule} from '@angular/common/http';
 import { HeaderComponent } from './home/header/header.component';
+import { SearchBarComponent } from './home/search-bar/search-bar.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,14 +24,19 @@ import { HeaderComponent } from './home/header/header.component';
     RecipeComponent,
     SmallRecipeListComponent,
     BigRecipeListComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CommonModule,
     MatIconModule,
-    HttpClientModule
+    MatFormFieldModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
