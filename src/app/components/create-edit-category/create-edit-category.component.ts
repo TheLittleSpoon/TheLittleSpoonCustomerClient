8006815eed54ded92bef8f18ec9acbf5345080aa
שירז בהר <shirazbehar@gmail.com>
2021-03-04 01:55:44 +0200

    create and edit recipe

--- src/app/components/create-edit-category/create-edit-category.component.ts
+++ src/app/components/create-edit-category/create-edit-category.component.ts
@@ -1,15 +1,108 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from 'src/app/services/categories.service';
+import { Category } from 'src/app/interfaces/category';
 
 @Component({
   selector: 'app-create-edit-category',
   templateUrl: './create-edit-category.component.html',
-  styleUrls: ['./create-edit-category.component.css']
+  styleUrls: ['./create-edit-category.component.css'],
 })
 export class CreateEditCategoryComponent implements OnInit {
+  @Input() isEditMode?: boolean = true;
+  @Input() category?: Category;
+  @Output() saveCategory!: EventEmitter<Category>;
+  @Output() updateCategory!: EventEmitter<Category>;
+  url?: string;
+  createCategoryForm!: FormGroup;
+  categoryToEdit?: Category;
+  categoryToEditChanged?: boolean;
+  submitted!: boolean;
+  showProgressBar!: boolean;
+  categories!: Category[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private categoryService: CategoryService,
+    private route: ActivatedRoute
+  ) {
+    this.saveCategory = new EventEmitter<Category>();
+    this.updateCategory = new EventEmitter<Category>();
+    this.categoryToEditChanged = true;
+    this.showProgressBar = false;
   }
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.categoryToEdit = this.categoryService.getCategory(params.id);
+    });
+    this.categories = this.categoryService.getCategories();
+    this.initForm();
+    this.createCategoryForm.valueChanges.subscribe((newCategory: Category) => {
+      const { name, imageUrl } = newCategory;
+      this.categoryToEditChanged =
+        name == this.categoryToEdit?.name &&
+        imageUrl == this.categoryToEdit?.imageUrl;
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  loadFile(event: any): void {
+    const selectedImage = event.target.files[0];
+    if (selectedImage) {
+      const reader = new FileReader();
+      reader.readAsDataURL(selectedImage);
+      reader.onload = (e: any) => {
+        this.url = e.target.result;
+      };
+    }
+  }
+
+  deleteUrl(): void {
+    this.url = '';
+    this.createCategoryForm.controls.imageFile?.setValue(undefined);
+  }
+
+  onSubmit(): void {
+    this.showProgressBar = true;
+    const category: Category = {
+      name: this.createCategoryForm.controls.name?.value,
+      imageUrl: this.url,
+    };
+    this.categoryToEdit
+      ? this.categoryService.updateCategory(category)
+      : this.categoryService.saveCategory(category);
+    this.categoryService.saveCategory(category);
+    this.createCategoryForm.reset();
+    this.deleteUrl();
+  }
+
+  private initForm(): void {
+    this.createCategoryForm = this.fb.group({
+      categoryName: new FormControl(
+        this.categoryToEdit ? this.categoryToEdit.name : '',
+        [Validators.required, Validators.minLength(3)]
+      ),
+      imageFile: new FormControl(
+        this.categoryToEdit ? this.categoryToEdit.imageUrl : ''
+      ),
+    });
+    this.categoryToEdit ? (this.url = this.categoryToEdit.imageUrl) : '';
+  }
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Category } from '../interfaces/category';
 import Swal from 'sweetalert2';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -21,17 +22,91 @@ export class CategoryService {
       imageUrl:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO6g04Gj7W1Vo_P6C55ZGtrayc7MYgqL7uhQ&usqp=CAU',
     },
+    {
+      id: 3,
+      name: 'Fishes',
+      imageUrl:
+        'https://www.masalakorb.com/wp-content/uploads/2016/08/Grilled-Fish-Indian-Recipe-V5.jpg',
+    },
+    {
+      id: 4,
+      name: 'Breakfast',
+      imageUrl:
+        'https://simply-delicious-food.com/wp-content/uploads/2018/10/breakfast-board-500x500.jpg',
+    },
+    {
+      id: 5,
+      name: 'Desserts',
+      imageUrl:
+        'https://preppykitchen.com/wp-content/uploads/2020/05/No-Bake-Strawberry-Cheesecake-feature-1140x1615.jpg',
+    },
+    {
+      id: 6,
+      name: 'Vegetarian',
+      imageUrl:
+        'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F35%2F2020%2F12%2F28%2Fveg-diet-plan-promo.jpg&q=85',
+    },
+    {
+      id: 7,
+      name: 'Pasta',
+      imageUrl:
+        'https://media.gettyimages.com/photos/pasta-plate-picture-id637214478?k=6&m=637214478&s=612x612&w=0&h=sU0ukGe_aYoztRgIIaVE9oem-b9JJs5V4ysCTiXwEDA=',
+    },
+    {
+      id: 8,
+      name: 'Chicken',
+      imageUrl:
+        'https://easychickenrecipes.com/wp-content/uploads/2019/03/cheesy-asparagus-stuffed-chicken-breast-recipe-4-of-9.jpg',
+    },
   ];
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
-  public getCategories(): Category[] {
+  getCategories(): Category[] {
     return this.categories;
   }
 
-  public deleteCategory(category: Category): Observable<Category> {
+  deleteCategory(category: Category): Observable<Category> {
     return this.http.post<Category>(
       'http://34.66.166.236:3000/api/categories/delete',
       category
     );
+  }
+
+  saveCategory(category: Category): void {
+    this.http
+      .post<Category>(
+        'http://34.66.166.236/:3000/api/categories/create',
+        category
+      )
+      .toPromise()
+      .then((data) => {
+        Swal.fire('Success', 'Category Saved!', 'success');
+        this.router.navigate(['home']);
+      })
+      .catch((error) => {
+        Swal.fire('Error', 'Could Not Save Category!', 'error');
+        this.router.navigate(['home']);
+      });
+  }
+
+  getCategory(id: number) {
+    return this.categories.find((category: Category) => category.id == id);
+  }
+
+  updateCategory(category: Category): void {
+    this.http
+      .post<Category>(
+        'http://34.66.166.236/:3000/api/categories/update',
+        category
+      )
+      .toPromise()
+      .then((data) => {
+        Swal.fire('Success', 'Category Saved!', 'success');
+        this.router.navigate(['home']);
+      })
+      .catch((error) => {
+        Swal.fire('Error', 'Could Not Save Category!', 'error');
+        this.router.navigate(['home']);
+      });
   }
 }
