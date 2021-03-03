35f8e09aabd6ceb00a664860c9a4e8842964d0e2
שירז בהר <shirazbehar@gmail.com>
2021-03-03 21:03:40 +0200

    show recipes catrgory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { CreateEditRecipeComponent } from './components/create-edit-recipe/create-edit-recipe.component';
 import { HomeComponent } from './components/home/home.component';
+import { SmallRecipeListComponent } from './components/recipe-list/small-recipe-list/small-recipe-list.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/interfaces/category';
 import { CategoryService } from 'src/app/services/categories.service';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { Recipe } from '../recipe/types/recipe';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-categories',
@@ -9,9 +12,37 @@ import { CategoryService } from 'src/app/services/categories.service';
 })
 export class CategoriesComponent implements OnInit {
   categories!: Category[];
-  constructor(private categoryService: CategoryService) {
+  recipesToShow: Recipe[] = [];
+  showRecipes!: boolean;
+  title?: string;
+  constructor(
+    private categoryService: CategoryService,
+    private recipesService: RecipeService
+  ) {
     this.categories = this.categoryService.getCategories();
+    this.showRecipes = false;
   }
 
   ngOnInit(): void {}
+
+  setRecipesToShow(category?: Category): void {
+    this.title = 'Recipes - ' + category?.name;
+    if (this.showRecipes) {
+      this.clearRecipes();
+    }
+    this.recipesToShow = this.recipesService.recipes.filter(
+      (recipe: Recipe) => recipe.categoryId == category?.id
+    );
+    if (this.recipesToShow.length > 0) {
+      this.showRecipes = true;
+    } else {
+      Swal.fire('Error', 'No Recipe Found', 'error');
+    }
+  }
+
+  clearRecipes(): void {
+    this.showRecipes = false;
+    this.recipesToShow = [];
+    this.title = '';
+  }
 }
