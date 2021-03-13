149bfd8a0ab13004a3891f767f4cfb3ce19ed74b
Addie Cohen <dushkit100@gmail.com>
2021-03-13 23:02:58 +0200

    fix category height

--- src/app/components/home/header/header.component.ts
+++ src/app/components/home/header/header.component.ts
@@ -2,8 +2,6 @@ import {Component, Input, OnInit, Output} from '@angular/core';
 import {Subject} from 'rxjs';
 import {AuthenticationService} from '../../../services/authentication.service';
 
-const ADMIN_CLIENT_PATH = 'http://34.68.121.245/';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -14,6 +12,7 @@ export class HeaderComponent implements OnInit {
   @Input() connectedUsers: number = 0;
   @Output() burgerClick: Subject<void> = new Subject<void>();
   isAdmin: boolean = false;
+  readonly adminClientPath: string = 'http://34.68.121.245/';
 
   constructor(private authenticationService: AuthenticationService) {
     this.authenticationService.currentUser.subscribe(value => this.isAdmin = value?.isAdmin ?? false);
@@ -34,8 +33,8 @@ export class HeaderComponent implements OnInit {
     const postMsg = JSON.parse(localStorage.getItem('currentUser') as string).token;
     const frame = document.getElementById('ifr');
     if (this.isIFrame(frame) && frame.contentWindow) {
-      frame.contentWindow.postMessage(postMsg, ADMIN_CLIENT_PATH);
+      frame.contentWindow.postMessage(postMsg, this.adminClientPath);
     }
-    window.location.href = ADMIN_CLIENT_PATH;
+    window.location.href = this.adminClientPath;
   }
 }
